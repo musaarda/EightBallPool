f108519c10a29e6e65d580b66e8c25a5dd6aa0a7
Musa Arda <musaarda@gmail.com>
2023-01-26 10:53:49 +0300

    1-ball-hit-by-the-cue
    BallHit class is defined.

---
+++ Assets/Script/Ball/BallHit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// BallHit is responsible for moving the Cue Ball (0) by Rigidbody.AddForce()
+/// </summary>
+public class BallHit : MonoBehaviour
+{
+
+    [SerializeField] private float impulse;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        // Caching
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Collision Trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ball is Hit by the Cue");
+        AddForceToRigidbody();
+    }
+
+
+    private void AddForceToRigidbody()
+    {
+        // direction vector
+        Vector3 direction = new Vector3(1f, 0f, 0f);
+        rb.AddForce(direction * impulse, ForceMode.Impulse);
+    }
+}
