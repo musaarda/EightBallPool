3efd8681dbee5b8fa867201342280e731c503fcb
Musa Arda <musaarda@gmail.com>
2023-02-06 11:33:28 +0300

    ball-physics
    Ball Physics adjusted with Physic Materials, Bounce Threshold and Pockets.

--- Assets/Script/Ball/Ball.cs
+++ Assets/Script/Ball/Ball.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Enums;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
 public class Ball : MonoBehaviour
 {
     public BallNumbersEnum ballNumber;
--- Assets/Script/Utils/BallUtils.cs
+++ Assets/Script/Utils/BallUtils.cs
@@ -25,6 +25,61 @@ namespace Utils
             return null;
         }
 
+        public static SphereCollider GetSphereColliderComponent(GameObject ball)
+        {
+            return ball.GetComponent<SphereCollider>();
+        }
+
+        public static Rigidbody GetRigidbodyComponent(GameObject ball)
+        {
+            return ball.GetComponent<Rigidbody>();
+        }
+
+        public static void StopTheBall(GameObject ball)
+        {
+            // Check the SphereCollider -> remove bounce material
+            SphereCollider sc = GetSphereColliderComponent(ball);
+            if (sc != null)
+            {
+                RemovePhysicsMaterial(sc);
+            }
+
+            // Check Rigidbody
+            Rigidbody rb = GetRigidbodyComponent(ball);
+            if (rb != null)
+            {
+                SetVelocity(rb, Vector3.zero);
+                SetDrag(rb, 1f);
+                SetUseGravity(rb, true);
+                RemoveConstraints(rb);
+            }
+        }
+
+        public static void RemovePhysicsMaterial(SphereCollider sc)
+        {
+            sc.material = null;
+        }
+
+        public static void SetVelocity(Rigidbody rb, Vector3 velocity)
+        {
+            rb.velocity = velocity;
+        }
+
+        public static void SetDrag(Rigidbody rb, float drag)
+        {
+            rb.drag = drag;
+            rb.angularDrag = drag;
+        }
+
+        public static void SetUseGravity(Rigidbody rb, bool useGravity)
+        {
+            rb.useGravity = useGravity;
+        }
+
+        public static void RemoveConstraints(Rigidbody rb)
+        {
+            rb.constraints = RigidbodyConstraints.None;
+        }
     }
 
 }
